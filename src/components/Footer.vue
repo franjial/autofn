b4bb53e1e76c174c0402a92e17a74d53236af013
franjial <franciscojesusjimenez@gmail.com>
2023-08-20 09:45:49 +0200

    actualización teléfono footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <b-nav class="navbar fixed-bottom navbar-dark bg-dark">
     <b-navbar-brand>Calle Cervantes 28 bajo Fernán Núñez (Córdoba)</b-navbar-brand>
-    <b-navbar-brand>Teléfono <a href="tel:690833811">690 833 811</a></b-navbar-brand>
+    <b-navbar-brand>Teléfono <a href="tel:957963778">957 963 778</a></b-navbar-brand>
   </b-nav>
 </template>
 
